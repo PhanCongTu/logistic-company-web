802e25ce6f5d7ac0c5cf8d33588bed530747864e
PhanCongTu <94525644+PhanCongTu@users.noreply.github.com>
2024-10-27 20:22:40 +0700

    Add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,31 +1,92 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { DropdownModule } from 'primeng/dropdown';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { RippleModule } from 'primeng/ripple';
+import { Router } from '@angular/router';
 
-interface City {
-  name: string;
-  code: string;
-}
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FormsModule, DropdownModule],
+  imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule, RippleModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  cities: City[] | undefined;
-
-  selectedCity: City | undefined;
-
-  ngOnInit() {
-      this.cities = [
-          { name: 'New York', code: 'NY' },
-          { name: 'Rome', code: 'RM' },
-          { name: 'London', code: 'LDN' },
-          { name: 'Istanbul', code: 'IST' },
-          { name: 'Paris', code: 'PRS' }
-      ];
-  }
+    items: MenuItem[] | undefined;
+    items2: MenuItem[] | undefined;
+    constructor(private router: Router) {}
+    ngOnInit() {
+        this.items2 = [
+            {
+                label: 'Home',
+                icon: 'pi pi-home',
+                command: () => {
+                    this.router.navigate(['/']);
+                }
+            } 
+        ]
+        this.items = [
+            {
+                label: 'Home',
+                icon: 'pi pi-home',
+                command: () => {
+                    this.router.navigate(['/']);
+                }
+            },
+            {
+                label: 'Features',
+                icon: 'pi pi-star'
+            },
+            {
+                label: 'Projects',
+                icon: 'pi pi-search',
+                items: [
+                    {
+                        label: 'Core',
+                        icon: 'pi pi-bolt',
+                        shortcut: '⌘+S'
+                    },
+                    {
+                        label: 'Blocks',
+                        icon: 'pi pi-server',
+                        shortcut: '⌘+B'
+                    },
+                    {
+                        label: 'UI Kit',
+                        icon: 'pi pi-pencil',
+                        shortcut: '⌘+U'
+                    },
+                    {
+                        separator: true
+                    },
+                    {
+                        label: 'Templates',
+                        icon: 'pi pi-palette',
+                        items: [
+                            {
+                                label: 'Apollo',
+                                icon: 'pi pi-palette',
+                                badge: '2'
+                            },
+                            {
+                                label: 'Ultima',
+                                icon: 'pi pi-palette',
+                                badge: '3'
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                label: 'Contact',
+                icon: 'pi pi-envelope',
+                badge: '3'
+            }
+        ];
+    }
 }
