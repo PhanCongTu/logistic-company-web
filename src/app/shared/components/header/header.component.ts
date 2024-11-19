dfde987d92758e9faa690c9ef7ccee94759efa34
PhanCongTu <94525644+PhanCongTu@users.noreply.github.com>
2024-11-20 00:52:15 +0700

    Update

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, signal, ViewChild, WritableSignal } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
 import { BadgeModule } from 'primeng/badge';
@@ -13,30 +13,30 @@ import { MenuModule } from 'primeng/menu';
 import { LocalStorageService } from '../../../core/services/local-storage.service';
 import { User } from '../../models/responses/user.model';
 import { ROUTES } from '../../../app.routes';
+import { ROLES } from '../../constants/app-constants.constant';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [
-    MenubarModule, 
-    BadgeModule, 
-    AvatarModule, 
-    InputTextModule, 
-    RippleModule, 
-    CommonModule, 
-    DropdownModule, 
-    MenuModule
-],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: 'app-header',
+    standalone: true,
+    imports: [
+        MenubarModule,
+        BadgeModule,
+        AvatarModule,
+        InputTextModule,
+        RippleModule,
+        CommonModule,
+        DropdownModule,
+        MenuModule
+    ],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
     readonly routes = ROUTES;
 
     items: MenuItem[] | undefined;
     avatarItems: MenuItem[] | undefined;
-    
-    userInfo: User | undefined;
+    userInfoSignal: WritableSignal<User | undefined> = signal(undefined);
 
     @ViewChild('avatarMenu') avatarMenu!: Menu;
 
@@ -49,93 +49,84 @@ export class HeaderComponent {
     ngOnInit() {
         // Get the user information from local storage
         this.localStorageService.userInfo$.subscribe(userInfo => {
-            this.userInfo = userInfo;
-          });
+            this.userInfoSignal.set(userInfo);
+            this.initializeMenuItems();
+        });
+    }
 
+    get isAdmin(): boolean {
+        return this.userInfoSignal()?.roles?.includes(ROLES.ROLE_ADMIN) || false;
+    }
+
+    get isUser(): boolean {
+        return this.userInfoSignal()?.roles?.includes(ROLES.ROLE_USER) || false;
+    }
+
+    initializeMenuItems() {
         this.items = [
-            { label: 'Home',
+            {
+                label: 'Home',
                 icon: 'pi pi-home',
                 command: () => {
                     this.router.navigate(['/']);
                 }
             },
             {
-                label: 'Features',
-                icon: 'pi pi-star'
-            },
-            {
-                label: 'Projects',
-                icon: 'pi pi-search',
+                label: 'Manage',
+                visible: this.isAdmin,
+                icon: 'fa-solid fa-list-check',
                 items: [
                     {
-                        label: 'Core',
-                        icon: 'pi pi-bolt',
-                        shortcut: '⌘+S'
+                        label: 'Warehouse',
+                        icon: 'fa-solid fa-warehouse',
+                        command: () => this.router.navigate([this.routes.adminWarehouse])
                     },
                     {
-                        label: 'Blocks',
-                        icon: 'pi pi-server',
-                        shortcut: '⌘+B'
+                        label: 'User',
+                        icon: 'fa-solid fa-users',
+                        command: () => this.router.navigate([this.routes.adminUser])
                     },
-                    {
-                        label: 'UI Kit',
-                        icon: 'pi pi-pencil',
-                        shortcut: '⌘+U'
-                    },
-                    {
-                        separator: true
-                    },
-                    {
-                        label: 'Templates',
-                        icon: 'pi pi-palette',
-                        items: [
-                            {
-                                label: 'Apollo',
-                                icon: 'pi pi-palette',
-                                badge: '2'
-                            },
-                            {
-                                label: 'Ultima',
-                                icon: 'pi pi-palette',
-                                badge: '3'
-                            }
-                        ]
-                    }
                 ]
             },
             {
-                label: 'Contact',
-                icon: 'pi pi-envelope',
-                badge: '3'
+                label: 'Feature',
+                visible: this.isUser,
+                icon: 'fa-regular fa-star',
+                items: [
+                    {
+                        label: 'My shipment',
+                        icon: 'fa-solid fa-truck',
+                        command: () => this.router.navigate([this.routes.adminWarehouse])
+
+                    },
+                ]
             }
         ];
 
         this.avatarItems = [
             { label: 'Profile', icon: 'pi pi-user', command: () => this.goToProfile() },
-            { label: 'Warehouse', icon: 'pi pi-user', command: () => this.router.navigate([this.routes.adminWarehouse]) },
-            { label: 'Users', icon: 'pi pi-user', command: () => this.router.navigate([this.routes.adminUser]) },
             { label: 'Settings', icon: 'pi pi-cog', command: () => this.goToSettings() },
-            { label: 'Logout', icon: 'pi pi-sign-out', command: () => this.logout(), 
-                styleClass: 'logoutItem' 
+            {
+                label: 'Logout', icon: 'pi pi-sign-out', command: () => this.logout(),
+                styleClass: 'logoutItem'
             }
-          ];
+        ];
     }
 
+    toggleMenu(event: MouseEvent) {
+        this.avatarMenu.toggle(event); // Hiển thị dropdown
+    }
 
-  toggleMenu(event: MouseEvent) {
-    this.avatarMenu.toggle(event); // Hiển thị dropdown
-  }
+    goToProfile() {
+        this.router.navigate([this.routes.userInfo]);
+    }
 
-  goToProfile() {
-    this.router.navigate([this.routes.userInfo]);
-  }
+    goToSettings() {
+        // Logic to navigate to settings
+    }
 
-  goToSettings() {
-    // Logic to navigate to settings
-  }
-
-  logout() {
-    this.localStorageService.removeUser();
-    this.router.navigate([this.routes.home]);
-  }
+    logout() {
+        this.localStorageService.removeUser();
+        this.router.navigate([this.routes.home]);
+    }
 }
--- src/app/shared/constants/app-constants.constant.ts
+++ src/app/shared/constants/app-constants.constant.ts
@@ -3,3 +3,8 @@ export class AppConstants {
       static readonly LOGIN_NAME_REGEX: RegExp = /^([a-zA-Z0-9._-]{6,16}$)/;
       static readonly EMAIL_REGEX: RegExp = /^[a-zA-Z0-9\+\.\_\%\-\+]{1,256}\@[a-zA-Z0-9][a-zA-Z0-9\-]{0,62}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{0,25})+$/;
 }
+
+export enum ROLES {
+      ROLE_ADMIN = "ROLE_ADMIN",
+      ROLE_USER = "ROLE_USER",
+}
